1390b1941d7c7431cff7d86751eda11182d19ddc
Mayuri Ashok Kamble <sweetyka1257@gmail.com>
2025-01-01 19:28:20 +0530

    Initial commit for PortfolioMayuri project

--- PortfolioMayuri/settings.py
+++ PortfolioMayuri/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Add this if you're preparing for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Ensure this directory exists
 
